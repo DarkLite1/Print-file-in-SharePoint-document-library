d8f63fc3ecaa5068c2e47230b9cd250484139795
DarkLite1 <BrechtGijbels@gmail.com>
2025-01-27 10:35:51 +0100

    update Example

--- Print file.ps1
+++ Print file.ps1
@@ -31,7 +31,7 @@ Param (
     [String]$SiteId = 'hcgroupnet.sharepoint.com,b4c482ba-d46d-4a40-93f1-463b40faacd4,213a6ffc-1009-43ca-be81-d20b54789765',
     [String]$DriveId = 'b!uoLEtG3UQEqT8UY7QPqs1PxvOiEJEMpDvoHSC1R4l2W5o0sx337DTZPZGtpfnBvg',
     [String]$FolderId = '01P4GU2YRPPOUAAVJA6ZC2IZMNPRNCVPFI',
-    [String]$PrinterName = 'BELPRBRAN603',
+    [String]$PrinterName = 'BELPRLIXH609',
     [Int]$PrinterPort = 9100
 
 )
